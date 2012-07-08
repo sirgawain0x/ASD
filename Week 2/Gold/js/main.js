76ad166a8f831d582051f3e38b17137b186b5c2b
Gawain Bracy II <g2musicgroup@fullsail.edu>
2012-07-08 10:57:19 -0400

    debugging code.
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

--- Week 2/Gold/js/main.js
+++ Week 2/Gold/js/main.js
@@ -51,14 +51,14 @@ $(document).ready(function() {
     function ne (x) {
         var theElement = $(x);
         return theElement;
-};
+    };
 
     // Create select field element and populate with options.
     var gpaRanges = ["--Choose Your GPA--","A: 4.0 - 3.5", "B: 3.4 - 2.5", "C: 2.4 - 1.5", "D: 1.4 - 1.0", "F: 0.9 - 0.0"];       
     
 
     //obj is the list of option values
-    function obj (){
+    function ranges (){
         var create = '<select id="groups">';
         for(var i = 0, j = gpaRanges.length; i < j; i++)
         {
@@ -66,8 +66,8 @@ $(document).ready(function() {
         }
         create += '</select>';
         $('#select').append(create);
-};
-    obj();
+    };
+    ranges();
 
     //Find Value of selected radio button
     function getSelectedRadio () {
@@ -79,7 +79,7 @@ $(document).ready(function() {
         };
     };
 
-//Find Value of selected checkbox
+    //Find Value of selected checkbox
     function getCheckbox () {
         var checkbox = $('#projectForm input.pop');
         for (var i = 0, j = checkbox.length; i < j; i++) {
@@ -126,10 +126,6 @@ $(document).ready(function() {
         alert("Information Saved!");
     };
 
-    var submit = $("input#submit").click(function() {
-        saveData();
-    });
-
     //Auto Populate Local Storage
     function autoFillData () {
         // The actual actual JSON OBJECT data required for this to work is coming from out JSON.js file which is loaded to out HTML page.
@@ -139,9 +135,20 @@ $(document).ready(function() {
             localStorage.setItem(id, JSON.stringify(json[c]));
         };
     };
+    
+    // get the image for the right category being displayed
+    function getImage (iconName, makeSubList) {
+        var imageLi = $('<li/>');
+        makeSubList.append(imageLi);
+        var newImg = $('<img/>');
+        var setSrc = newImg.attr('src', "images/icons"+ iconName +".png");
+        imageLi.append(newImg);
+     };
+     
+     
 
     function getData () {
-        toggleControls("on");
+       // toggleControls("on");
         if (localStorage.length === 0) {
             alert("There is no data in Local Storage so default data was added.");
             autoFillData();
@@ -150,15 +157,15 @@ $(document).ready(function() {
         $.mobile.changePage('#displaydata'); // go to page to display list
     };
         //write data from local storage to browser.
-        var makeDiv = $('<div></div>');
+        var makeDiv = $('<div/>');
         makeDiv.attr("id", "items");
         makeDiv.attr("data-role", "content");
         makeDiv.attr("data-theme", "d");
 
-        var makeDivPrimary = $("<div></div>");
+        var makeDivPrimary = $("<div/>");
         makeDivPrimary.attr("class", "content-primary");
 
-        var makeList = $('<ul></ul>');
+        var makeList = $('<ul/>');
         makeList.attr("id", "one");
         makeList.attr("data-role", "listview");
         makeList.attr("data-filter", "true");
@@ -166,36 +173,222 @@ $(document).ready(function() {
 
         makeDiv.append(makeList);
         makeDivPrimary.append(makeList);
-        $.body.append(makeDiv);
-        $("items").style.display = "block";
+        $(document.body).append(makeDiv);
+        $("items").css("display", "block");
         for (var i = 0, ls = localStorage.length; i < ls; i++) {
-            var makeli = document.createElement('li');
+            var makeli = $('<li/>');
             makeli.attr("id", "two");
-            var linksli = document.createElement('li');
-            linksLi.attr("id", "three");
-            makeList.appendChild(makeli);
+            var linksli = $('<li/>');
+            linksli.attr("id", "three");
+            makeList.append(makeli);
             var key = localStorage.key(i);
             var value = localStorage.getItem(key);
             //convert string from local storage value back to an object by using JSON.parse()
             var obj = JSON.parse(value);
-            var makeSubList = document.createElement('ul');
+            var makeSubList = $('<ul/>');
             makeSubList.attr("href", "#");
             makeSubList.attr("id", "four");
             
-            makeli.appendChild(makeSubList);
+            makeli.append(makeSubList);
             
             getImage(obj.group[1], makeSubList);
             for(var b in obj){
-                var makeSubli = document.createElement('li');
-                makeSubList.appendChild(makeSubli);
+                var makeSubli = $('<li/>');
+                makeSubList.append(makeSubli);
                 var optSubText = obj[b][0] + " " + obj[b][1];
-                makeSubli.innerHTML = optSubText;
-                makeSubList.appendChild(linksli);
+                makeSubli.html = optSubText;
+                makeSubList.append(linksli);
             };
             makeItemLinks(localStorage.key(i), linksli ); // Create our edit and delete links/buttons for each item in local storage.
         };
 
     $("#displayLink").click(function(){
         autoFillData();
+    });
+    
+    //Make Item Links
+    //Create the edit and delete links for each stored item when displayed.
+    function makeItemLinks (key, linksli) {
+        //Edit single item link
+        var editLink = $("<a/>");
+            editLink.href = '#';
+        editLink.key = key;
+        var editText = "Edit Information";
+        editLink.click(function(){
+            editItem();
+        });
+        editLink.html = editText;
+        linksli.append(editLink);
+ 
+        //Add line break
+        var breakTag = $('<br/>');
+        linksli.append(breakTag);
+ 
+        var deleteLink = $('<a/>');
+            deleteLink.href = '#';
+        deleteLink.key = key;
+        var deleteText = "Delete Information";
+        deleteLink.click(function(){
+            deleteItem();
+        });
+        deleteLink.html = "Delete Text";
+        linksli.append(deleteLink);
+ 
+    
+    function editItem () {
+        //Grab data from our item from local storage
+        var value = localStorage.getItem(this.key);
+        var obj = JSON.parse(value);
+ 
+        //show form
+        //toggleControls("off");
+ 
+        //Populate the form fields with current localStorage values.
+        $('fname').val = obj.fname[1];
+        $('lname').val = obj.lname[1];
+        $('email').val = obj.email[1];
+        var radios = $(document.forms[0].sex);
+        for(var i = 0, r = radios.length; i < r; i++){
+            if (radios[i].val() == "male" && obj.sex[1] == "male"){
+                radios[i].attr("checked", "checked");
+            }else if(radios[i].val() == "female" && obj.sex[1] == "female"){
+                radios[i].attr("checked", "checked");
+            };
+        };
+        $('groups').val = obj.group[1];
+ 
+        var check = $(document.forms[1].pop);
+        for (var a = 0, c = check.length; i < c; i++) {
+            if(check[i].val == "Small" && obj.pop[1] == "Small") {
+                $('small').attr("checked", "checked");
+            }else if(check[i].val() == "Medium" && obj.pop[1] == "Medium") {
+                $('medium').attr("checked", "checked");
+            }else if(check[i].val() == "Large" && obj.pop[1] == "Large") {
+                $('large').attr("checked", "checked");
+            };
+        };
+        $('#comments').val = obj.interests[1];
+         // Remove the initial listener from the input 'submit' button.
+        save.off.click(function() {
+            saveData();
+        });
+ 
+        // Change submit button value to edit button
+        $('#submit').val = "Edit Information";
+        var editSubmit = $('#submit');
+        //Save the key value established in this function as a property of the editSumbit event
+        //so we can use that value when we save the data we edited.
+        editSubmit.click(function(){
+            validate();
+        });
+        editSubmit.key = this.key;
+    };
+
+    function deleteItem () {
+        var ask = confirm("Are you sure you want to delete thins information?");
+        if (ask) {
+            localStorage.removeItem(this.key);
+            alert("Information successfully deleted.");
+            window.location.reload();
+        } else{
+            alert("Information was not deleted.");
+        };
+    };
+ 
+    function clearLocal () {
+        if (localStorage.length === 0){
+            alert("There is no data to clear.");
+        }else{
+            localStorage.clear();
+            alert("All data is deleted.");
+            window.location.reload();
+            return false;
+        };
+    };
+
+    function validate (data) {
+        //Define the elements we want to check
+        var getFname = $('fname');
+        var getLname = $('lname');
+        var getEmail = $('email');
+        var getGroup = $('groups');
+        var getInterests = $('comments');
+ 
+        // Reset error messages
+        errMsg.html = " ";
+        getFname.css('border', '1px solid black');
+        getLname.css('border', '1px solid black');
+        getEmail.css('border', '1px solid black');
+        getGroup.css('border', '1px solid black');
+        getInterests.css('border', '1px solid black');
+         
+ 
+        //Get Error Messages
+        var messageAry = [];
+         
+        // First Name Validation
+        if (getFname.val === ""){
+            var fNameError = "Please enter a first name.";
+            getFname.css('border','1px solid red');
+            messageAry.push(fNameError);
+        };
+ 
+        //Last Name Validation
+        if (getLname.val === ""){
+            var lNameError = "Please enter a last name.";
+            getLname.css('border','1px solid red');
+            messageAry.push(lNameError);
+        };
+ 
+        //Email Validation
+        var reg = /^\w+([\.]?\w+)*@\w+([\.]?\w+)*(\.\w{2,3})+$/;
+            if(!(reg.exec(getEmail.val))){
+                var emailError = "Please enter a valid email address.";
+                getEmail.css('border','1px solid red');
+                messageAry.push(emailError);
+        };
+ 
+        //Gpa Validation
+        if (getGroup.value === "--Choose Your GPA--"){
+            var groupError = "Please select your GPA.";
+            getGroup.css('border','1px solid red');
+            messageAry.push(groupError);
+        };
+         
+        //Interests Validation
+        if (getInterests.value === ""){
+            var interestsError = "Please type your intrest(s).";
+            getInterests.css('border','1px solid red');
+            messageAry.push(interestsError);
+        };
+
+        // If there are errors display them on screen
+        if(messageAry.length >= 1){
+            for (var i = 0, j = messageAry.length; i < j; i++) {
+                var txt = $('<li/>');
+                txt.html = messageAry[i];
+                errMsg.append(txt);
+            };
+            data.preventDefault();
+            return false;
+        }else{
+            //If all is ok save data. Send the key value (which came from the editData function).
+            //Remember this key value was passed through the editSubmit event Listener as a property.
+            saveData(this.key);
+ 
+        };
+    };
+
+    // Submit Link & Submit Click Events
+    var displayLink = $("#displayLink").click(function(){
+        getData();
+    });
+
+    var clearLink = $("#clear").click(function(){
+        clearLocal();
+    });
+
+    var submit = $("input#submit").click(function() {
+        saveData();
     });
 });
