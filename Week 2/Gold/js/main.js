2dfaffc737b916202575a47239e31cd466cf9597
Gawain Bracy II <g2musicgroup@fullsail.edu>
2012-07-12 10:43:13 -0400

    Working on getting buttons working
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

--- Week 2/Gold/js/main.js
+++ Week 2/Gold/js/main.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
             create += '<option value="'+gpaRanges[i]+'">'+gpaRanges[i]+'</option>';
         }
         create += '</select>';
-        $('#select').append($(create);
+        $('#select').append($(create));
     };
     ranges();
 
@@ -90,7 +90,7 @@ $(document).ready(function() {
     function saveData (key) {
     
         // If there is no key this means this is a brand new item and we need a brand new key
-        if ($(!key).val() == ''){
+        if ($(!key).val() === ''){
          
         //Gather up all form filled values and store in object.
         var id   = Math.floor(Math.random()* 10000001);
@@ -136,10 +136,10 @@ $(document).ready(function() {
     // get the image for the right category being displayed
     function getImage (iconName, makeSubList) {
         var imageLi = $('<li/>');
-        makeSubList.append($(imageLi);
+        makeSubList.append($(imageLi));
         var newImg = $('<img/>');
         var setSrc = newImg.attr('src', "images/icons"+ iconName +".png");
-        imageLi.append($(newImg);
+        imageLi.append($(newImg));
      };
      displayLink(localStorage);
      
@@ -175,15 +175,15 @@ $(document).ready(function() {
         makeList.attr("data-inset", "true");
 
         makeDiv.append($(makeList));
-        makeDivPrimary.append($(makeList);
-        $(document.body).append($(makeDiv);
+        makeDivPrimary.append($(makeList));
+        $(document.body).append($(makeDiv));
         $("#items").css({"display": "block"});
         for (var i = 0, ls = localStorage.length; i < ls; i++) {
             var makeli = $('<li></li>');
             makeli.attr("id", "two");
             var linksli = $('<li></li>');
             linksli.attr("id", "#three");
-            makeList.append($(makeli);
+            makeList.append($(makeli));
             var key = localStorage.key(i);
             var value = localStorage.getItem(key);
             //convert string from local storage value back to an object by using JSON.parse()
@@ -192,15 +192,15 @@ $(document).ready(function() {
             makeSubList.attr("href", "#");
             makeSubList.attr("id", "#four");
             
-            makeli.append($(makeSubList);
+            makeli.append($(makeSubList));
             
             getImage(obj.group[1], makeSubList);
             for(var b in obj){
                 var makeSubli = $('<li/>');
-                makeSubList.append($(makeSubli);
+                makeSubList.append($(makeSubli));
                 var optSubText = obj[b][0] + " " + obj[b][1];
                 makeSubli.html = optSubText;
-                makeSubList.append($(linksli);
+                makeSubList.append($(linksli));
             };
             makeItemLinks(localStorage.key(i), linksli ); // Create our edit and delete links/buttons for each item in local storage.
         };
@@ -217,11 +217,11 @@ $(document).ready(function() {
             editItem();
         });
         editLink.html = editText;
-        linksli.append($(editLink);
+        linksli.append($(editLink));
  
         //Add line break
         var breakTag = $('<br/>');
-        linksli.append($(breakTag);
+        linksli.append($(breakTag));
  
         var deleteLink = $('<a/>');
             deleteLink.href = '#';
@@ -232,7 +232,7 @@ $(document).ready(function() {
         });
         deleteLink.html = "Delete Text";
     };
-        linksli.append($(deleteLink);
+        linksli.append($(deleteLink));
  
     
     function editItem () {
@@ -368,7 +368,7 @@ $(document).ready(function() {
             for (var i = 0, j = messageAry.length; i < j; i++) {
                 var txt = $('<li/>');
                 txt.html = messageAry[i];
-                errMsg.append($(txt);
+                errMsg.append($(txt));
             };
             data.preventDefault();
             return false;
@@ -387,3 +387,10 @@ $(document).ready(function() {
 
     $('#submit').on('click', saveData);
 });
+
+
+
+
+
+
+
