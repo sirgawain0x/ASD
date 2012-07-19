58c11a6fb1c50537fc59d70f7734e2fecd40bcba
Gawain Bracy II <g2musicgroup@fullsail.edu>
2012-07-19 12:54:13 -0400

    added previous interests to app from couchdb
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

--- ASDcouch/ASDcouch/app/_attachments/js/main.js
+++ ASDcouch/ASDcouch/app/_attachments/js/main.js
@@ -3,7 +3,22 @@ $(document).ready(function(){
 		"url": "_view/students",
 		"dataType": "json",
 		"success": function(data){
-			console.log(data);
+			$.each(data.rows, function(index, students){
+				var fname = students.value.fname;
+				var lname = students.value.lname;
+				var email = students.value.email;
+				var sex = students.value.sex;
+				var group = students.value.sex;
+				var pop = students.value.pop;
+				var interests = students.value.interests;
+				$('#studentlist').append(
+					$('<li>').append(
+						$('<a>').attr('href', '#')
+							.text(interests)
+					)
+				);
+			});
+			$('#studentlist').listview('refresh');
 		} 
 	});
 });
