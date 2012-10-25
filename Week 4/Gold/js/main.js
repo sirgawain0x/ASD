96ec57b5ab06f7a7d6d78dae79aa6528be5ddddf
Gawain Bracy II <g2musicgroup@fullsail.edu>
2012-10-25 12:24:18 -0400

    Updated
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

--- Week 4/Gold/js/main.js
+++ Week 4/Gold/js/main.js
@@ -210,6 +210,23 @@ $(document).ready(function() {
             };
             makeItemLinks(localStorage.key(i), linksli ); // Create our edit and delete links/buttons for each item in local storage.
         };
+
+        // Create select field element and populate with options.
+    function makeElement () {
+        var formTag = document.getElementsByTagName("form"), // formTag is an array of all the form tags.
+            selectLi = ne("select"),
+            makeSelect = document.createElement('select');
+            makeSelect.setAttribute("id", "groups");
+        for(var i = 0, j = gpaRanges.length; i < j; i++){
+            var makeOption = document.createElement('option');
+            var optText = gpaRanges[i];
+            makeOption.setAttribute("value",optText);
+            makeOption.innerHTML = optText;
+            makeSelect.appendChild(makeOption);
+        };
+        selectLi.appendChild(makeSelect);
+    };
+    makeElement();
     
     //Make Item Links
     //Create the edit and delete links for each stored item when displayed.
